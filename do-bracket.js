1581ef3aa7f7bc82147ff749807670cde647636d
ImranSanvi <imranmohammed5220@gmail.com>
2025-09-23 20:35:56 +0600

    Dot vs Bracket

---
+++ do-bracket.js
@@ -0,0 +1,25 @@
+
+
+const student = {
+    name: 'Imran',
+    department: 'CSE',
+    age: 25,
+    1: 18,
+    2: 145,
+    'location' : "Cox's Bazar"
+}
+
+// dot notation
+const studentName = student.name
+const studentLocation = student.location
+const studentDepartment = student.department
+const output = `student name is ${studentName} his department is ${studentDepartment} and location ${studentLocation}`
+console.log(output)
+
+
+// Bracket notation
+const studentOne = student['1']
+const studentTwo = student[2]
+const studentDept = student['department']
+const studentLocation2 = student['location']
+console.log(studentOne, studentTwo, studentDept, studentLocation2)
