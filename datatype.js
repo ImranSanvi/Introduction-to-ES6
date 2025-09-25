eb6a1208cb23e9e6db567d215a7213909252df5b
ImranSanvi <imranmohammed5220@gmail.com>
2025-09-25 14:24:24 +0600

    datatype

---
+++ datatype.js
@@ -0,0 +1,32 @@
+
+let x = 123
+console.log(x, typeof(x));
+
+x = 12.3
+console.log(x, typeof(x));
+
+x = 12376482349009435642347893256234643n
+console.log(x, typeof(x));
+
+x = 'Imran'
+console.log(x, typeof(x));
+
+x = true
+console.log(x, typeof(x));
+
+x = null
+console.log(x, typeof(x));
+
+let y
+console.log(y, typeof(y));
+
+
+// Non primitive datatype
+x = [1, 2, 3]
+console.log(x, typeof(x));
+
+x = {name:'Imran', age: 25}
+console.log(x, typeof(x));
+
+x = new Date()
+console.log(x, typeof(x));
